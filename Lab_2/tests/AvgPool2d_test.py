b8bf9ac7d7526522327d1b683a029597e923cbf3
Danil Tereshin <yaterya7@yandex.ru>
2024-09-16 12:33:29 +0300

    Add backproptest for AvgPool2d

--- Lab_2/tests/AvgPool2d_test.py
+++ Lab_2/tests/AvgPool2d_test.py
@@ -1,8 +1,6 @@
 from NN_utils.Layers.AvgPool2d import AvgPool2d
 import numpy as np
 
-# FEEDFORWARD
-#######################################################
 kernel_size = 2
 stride = 2
 padding = 1
@@ -28,4 +26,28 @@ expected_output = np.array([[[0.25, 1.25],
                              [0, 0.25],
                              [0.25, 0.5]]])
 
+# feedforward test
 np.testing.assert_array_equal(output_data, expected_output)
+
+delta = np.array([[[1, 2],
+                   [3, 4],
+                   [5, 6]],
+
+                  [[7, 8],
+                   [9, 10],
+                   [11, 12]]])
+
+delta_input = layer.backprop(delta, 1e-3)
+
+expected_delta_input = np.array([[[0.25, 0.5, 0.5],
+                                  [0.75, 1, 1],
+                                  [0.75, 1, 1],
+                                  [1.25, 1.5, 1.5]],
+
+                                 [[1.75, 2, 2],
+                                  [2.25, 2.5, 2.5],
+                                  [2.25, 2.5, 2.5],
+                                  [2.75, 3, 3]]])
+
+# backprop testing
+np.testing.assert_array_equal(delta_input, expected_delta_input)
